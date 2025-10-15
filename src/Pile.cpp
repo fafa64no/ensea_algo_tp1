9119e760e54bfc3ebba044d5707ee79dd890c2ca
Sébastien Nouira Huet <148809822+fafa64no@users.noreply.github.com>
2025-10-15 13:46:24 +0200

    [ADD] main, cmake and src folder

---
+++ src/Pile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sebas on 10/15/25.
+//
+
+#include "Pile.h"
---
+++ src/Pile.h
@@ -0,0 +1,12 @@
+//
+// Created by sebas on 10/15/25.
+//
+
+#ifndef ENSEA_ALGO_TP1_PILE_H
+#define ENSEA_ALGO_TP1_PILE_H
+
+
+class Pile {};
+
+
+#endif //ENSEA_ALGO_TP1_PILE_H
